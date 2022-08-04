2543431749500af629da778006b38eb7c9b22b28
N3WC0DER <sawkin.nik@gmail.com>
2022-08-04 18:43:10 +0400

    hotfix: I'm a fool. Forgot to test the examples. Already fixed.-a

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <unistd.h>
 
 #include "ThreadManager.h"
 
@@ -21,7 +22,7 @@ int add(int a, int b) {
 }
 
 struct A {
-	operator()(const std::string str) {
+	void operator()(const std::string str) {
 		std::scoped_lock<std::mutex> lock(mutex);
 		std::cout << str << std::endl;
 	}
@@ -36,7 +37,7 @@ public:
 				b[i] = rand() % 100;
 	}
 	
-	operator()(const int i) {
+	int& operator()(const int i) {
 		return b[i];
 	}
 	
@@ -67,24 +68,27 @@ int main() {
 	
 	// A lambda with two parameters that return their sum in the third parameter
 	int res;
-	future = thm->addTask([] (int a, int b, int& result) {
+	auto future2 = thm->addTask([] (int a, int b, int& result) {
 		result = a + b;
 	}, 10, 37, std::ref(res));
 	
 	// Can be used to wait for a task to complete
-	future.get();
+	future2.get();
 	std::cout << "10 + 37 = " << res << std::endl;
 	
 	// Unary functor
 	A a;
-	thm->addTask(&a, "Functor");
+	thm->addTask(a, "Functor");
 	
 	// Unary functor and member function
 	B b;
 	thm->addTask(&B::print, &b); // Do not forget that a non-static member function takes as its first argument a reference to the object from which it is called.
-	future = thm->addTask(&b, 3);
-	future.get() = 110;
+	auto future3 = thm->addTask(b, 3);
+	std::cout << future3.get() << std::endl;
 	thm->addTask(&B::print, &b);
 	
+	sleep(1); // To complete all tasks
+	thm->free();
+	std::cout << "Main" << std::endl;
 	return 0;
 }
